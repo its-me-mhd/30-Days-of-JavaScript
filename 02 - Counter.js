cea55803157f5a133905b7ad52aa6bfc14e85e4b
it's me <mohamedabdirisaaq57@gmail.com>
2024-11-01 16:20:43 +0600

    Update 02 - Counter.js

--- 02 - Counter.js
+++ 02 - Counter.js
@@ -4,10 +4,12 @@
 
 // (----------------------------- THE ANSWER -----------------------------)
 
-
 var createCounter = function(n) {
+    
+    let count = n;
+
     return function() {
-        return n++;
+        return count ++ ;
     };
 };
 

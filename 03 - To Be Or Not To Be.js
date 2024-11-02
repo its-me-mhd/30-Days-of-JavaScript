4837c2daaefac6665616d942cf8b2aa256c0dcb5
it's me <mohamedabdirisaaq57@gmail.com>
2024-11-02 08:47:16 +0600

    Update 03 - To Be Or Not To Be.js

--- 03 - To Be Or Not To Be.js
+++ 03 - To Be Or Not To Be.js
@@ -4,15 +4,3 @@
 
 // (----------------------------- THE ANSWER -----------------------------)
 
-var expect = function(val) {
-    return {
-        toBe: (val2) => {
-            if (val !== val2) throw new Error("Not Equal");
-            else return true;
-        },
-        notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal");
-            else return true;
-        }
-    }
-};
